064e404b8677fc475123f86bd82421d9e4e8138f
kameron Jackson <kameronjackson28@gmail.com>
2021-03-16 11:01:31 -0500

    pushing for github

---
+++ Basic_C#_Programs/HelloWorld Program/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld_Program
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!\n This is Kameron");
+            Console.ReadLine();
+        }
+    }
+}
