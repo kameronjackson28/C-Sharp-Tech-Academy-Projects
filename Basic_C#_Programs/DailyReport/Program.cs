b5658816b1bcf878700fc5af4c0c263f5360f050
kameron Jackson <kameronjackson28@gmail.com>
2021-03-17 13:38:49 -0500

    git

---
+++ Basic_C#_Programs/DailyReport/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //gives opening headline
+            Console.WriteLine("The Tech Academy\nStudent Daily Report");
+            //prints question asked "What is your name?"
+            Console.WriteLine("What is your name?");
+            //takes input from user 
+            string yourName = Console.ReadLine();
+            //prints question 
+            Console.WriteLine("What course are you on?");
+            //takes input from user stores it as string data
+            string course = Console.ReadLine();
+            //takes question asked prints to console
+            Console.WriteLine("What page number?");
+            // takes only integer value from user
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+            // asks question to console
+            Console.WriteLine("Do you need help with anything? Please answer\" True\" or \"False");
+            //takes only true or false input from user
+            Convert.ToBoolean(Console.ReadLine());
+           
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            //takes string input from user saves data as string
+            string experiences = Console.ReadLine();
+            //Asks question prints to console.
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            // saves string input from user
+            string feedback = Console.ReadLine();
+            //Asks question prints to console
+            Console.WriteLine("How many hours did you study today?");
+            //takes integer answer only saves data
+            int hoursStudied = Convert.ToInt32(Console.ReadLine());
+            //Takes last line and closes program
+            Console.WriteLine("Thank you for all of your answers. An Instructor will respond to this shortly. Have a great day!");
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/VariableAndDataTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VariableAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is:" + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //int favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+
+        }
+    }
+}
