823138669c9e4084de6309f699c83a6f0ae5800f
kameron Jackson <kameronjackson28@gmail.com>
2021-03-26 12:38:46 -0500

    Update Program.cs

--- Basic_C#_Programs/Branching/Program.cs
+++ Basic_C#_Programs/Branching/Program.cs
@@ -24,30 +24,39 @@ namespace Branching
             //according to "if" statement, if variable userweight is not greater than variable weight it moves to the else statement.
             else
             {
-                //
+                //prints what's in parenthese to console
                 Console.WriteLine("Please enter the package width:");
+                //assigns int width variable of user's input
                 int width = Convert.ToInt32(Console.ReadLine());
 
-
+                //prints what's in parenthese to console
                 Console.WriteLine("Please enter the package height");
-
-
+                //assigns integer variable to user's input
                 int height = Convert.ToInt32(Console.ReadLine());
+                //prints what's in parenthese to console
                 Console.WriteLine("Please enter the package length:");
+                //assigns int variable to user's input 
                 int length = Convert.ToInt32(Console.ReadLine());
+                //adds variables length, width, and height assigning it to a new variable
                 int dimensions = length + width + height;
+                //assigns the number 50 as correctdim variable.
                 int correctdim = 50;
-
+                //creates an if statement comparing dimensions and correct dimensions
                 if (dimensions > correctdim)
                 {
+                    //prints what's in parenthese to console
                     Console.WriteLine("Package too big to be shipped via Package Express.");
-                    Console.ReadLine();
+                    
 
                 }
+                //according to "if" statement, if variable correctdim is not greater than variable dimensions it moves to the else statement.
                 else
                 {
+                    //assigns length * width * height * userweight to decimal variable total
                     decimal total = Convert.ToDecimal(length * width * height * userweight);
+                    //takes the decimal variable total and divides it by 100 creating new variable total1
                     decimal total1 = total / 100;
+                    //concatenates "Your estimated total for shipping this package is: " with a "$" finally, with variabel total1
                     Console.WriteLine("Your estimated total for shipping this package is: "+ "$" + total1);
                 }
 
