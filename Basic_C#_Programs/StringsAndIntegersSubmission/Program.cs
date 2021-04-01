2a02698d1107e4b9651f1a30f49ea7585e8587c4
kameron Jackson <kameronjackson28@gmail.com>
2021-04-01 18:44:10 -0500

    uploAD

--- Basic_C#_Programs/StringsAndIntegersSubmission/Program.cs
+++ Basic_C#_Programs/StringsAndIntegersSubmission/Program.cs
@@ -7,17 +7,39 @@ namespace StringsAndIntegersSubmission
     {
         static void Main(string[] args)
         {
-            List<int> first = new List<int>() { 5, 3, 2, 6, 10 };
-            Console.WriteLine("Please enter a number to divide by:");
-            int user = Convert.ToInt32(Console.ReadLine());
-            
-           foreach( int n in first)
             {
-                int test = n / user;
-                Console.WriteLine(test);
-                
-            }
+                try
+                {
+                    List<int> first = new List<int>() { 5, 3, 2, 6, 10 };
+                    Console.WriteLine("Please enter a number to divide by:");
+                    int user = Convert.ToInt32(Console.ReadLine());
 
+                    foreach (int n in first)
+                    {
+
+
+
+                        int test = n / user;
+                        Console.WriteLine(test);
+
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Please enter an integer that's not zero or not a string and try again");
+                }
+
+                finally
+                {
+                    Console.ReadLine();
+
+                }
+                    Console.WriteLine("You've successfully ended the program");
+                }
+
+            }
         }
     }
-}
+
+
