e21346b764c89702f2db8fcab39bb7e094cbe1fb
kameron Jackson <kameronjackson28@gmail.com>
2021-04-02 20:12:15 -0500

    up

--- Basic_C#_Programs/Methods/Operators.cs
+++ Basic_C#_Programs/Methods/Operators.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace Methods
 {
-    class Objects
+    class Operators
     {
+        //creates method named multiply
+        public int multiply(int x)
+        {
+            // returns 5 * x which is userNum
+            return (5 * x);
+        }
+
+        //creates method named subtract
+        public int subtract(int x)
+        {
+            //returns 7 - x which is userNum
+            return (7 - x);
+        }
+
+        //creates method named add
+        public int add(int x)
+        {
+            //returns 5+x which is userNum
+            return (5 + x);
+        }
     }
 }
--- Basic_C#_Programs/Methods/Program.cs
+++ Basic_C#_Programs/Methods/Program.cs
@@ -7,9 +7,37 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            int userNum = Convert.ToInt32(Console.ReadLine());
+            //asks user to input number
             Console.WriteLine("Please enter a number:");
-            Console.WriteLine(Operators.add());
+
+            //assigns user input to userNum int
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            //instantiates the Operators class
+            Operators myMath = new Operators();
+
+            //calls the add method using userNum as x
+            int sum = myMath.add(userNum);
+            //calls the subtract method using userNum as x
+            int sum1 = myMath.subtract(userNum);
+            //calls the multiply method using userNum as x 
+            int remainder = myMath.multiply(userNum);
+            //prints multiply method to console.
+            Console.WriteLine(remainder);
+            //prints the add method to console.
+            Console.WriteLine(sum);
+            //prints the subtract method to console.
+            Console.WriteLine(sum1);
+
+            Console.ReadLine();
+
+            
+
+
+
+
+
+
         }
     }
 }
