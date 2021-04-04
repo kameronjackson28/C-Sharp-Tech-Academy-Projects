0bd9ff6cd1ab53a3e06bb638800a60f693483577
kameron Jackson <kameronjackson28@gmail.com>
2021-04-04 15:20:29 -0500

    uploading

--- Basic_C#_Programs/ClassSubmission/Division.cs
+++ Basic_C#_Programs/ClassSubmission/Division.cs
@@ -6,5 +6,43 @@ namespace ClassSubmission
 {
     class Division
     {
+        //creates a void method named mathDivide using int i as parameter
+        public void mathDivide(int i)
+        {
+            
+            //assigns i / 2 as value to result variable.
+            int result = i / 2;
+            //prints variable result to console.
+            Console.WriteLine(result);
+        }
+
+        //creates void method mathMultiply using out int x as a parameter
+        public void mathMultiply(out int x)
+        {
+            //assigns 5 to variable x
+            x = 5;
+            // assigns math equation x * 25 to variable secondResult
+            int secondResult = x * 25;
+            //prints secondResult to console.
+            Console.WriteLine("Here is your result " + secondResult);
+        }
+
+        //creates static method myMath using an int as the parameter
+        public static int myMath(int l)
+        {
+            //returns l * 6
+            return (l * 6);
+        }
+
+        //creates another myMath method(method overloading) using a string as the parameter
+        public static int myMath( string z)
+        {
+           //assigns string as an integer by converting said string to an integer giving it the variable name of a
+            int a = Convert.ToInt32(z);
+            //returns a * 7
+            return (a * 7);
+
+          
+        }
     }
 }
--- Basic_C#_Programs/ClassSubmission/Program.cs
+++ Basic_C#_Programs/ClassSubmission/Program.cs
@@ -6,7 +6,40 @@ namespace ClassSubmission
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Prints what's in parentheses to screen
+            Console.WriteLine("Hello, Welcome to the Class Submission Program");
+
+            //instantiates Division class 
+            Division division = new Division();
+
+            //Asks user to enter a number
+            Console.WriteLine("Please enter a number");
+            //converts user input to int assigning that value to int variable "i"
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            //calls the mathDivide method using parameter "i"
+            division.mathDivide(i);
+            //allows program to stay open for a second before moving to next item.
+            Console.ReadLine();
+
+            //declares local variable x whose value has already been assigned in it's method
+            int x;
+            //calls mathMultiply method using out parameter x
+            division.mathMultiply(out x);
+
+          
+            //assigns called method using 55 as parameter to variable sum
+            int sum = Division.myMath(55);
+            //prints variable sum to screen
+            Console.WriteLine(sum);
+
+            //assigns called method myMath assigning "7" as parameter to variable total
+            int total = Division.myMath("7");
+            //prints variable total to screen
+            Console.WriteLine(total);
+
+            //makes user press a key before program goes away.
+            Console.ReadLine();
         }
     }
 }
