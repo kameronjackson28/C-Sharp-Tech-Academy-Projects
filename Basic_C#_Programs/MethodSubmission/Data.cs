bfa20e17394056485e2a08b9c476d40fff60cf3e
kameron Jackson <kameronjackson28@gmail.com>
2021-04-03 20:30:40 -0500

    up

---
+++ Basic_C#_Programs/MethodSubmission/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MethodSubmission
+{
+    class Data
+    {
+        public static int mathOp(int y, int z = 4)
+        {
+            return (y * z * 25);
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodSubmission/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the beginning of the program");
+            Data data = new Data();
+
+            Console.WriteLine("Please input two numbers, one at a time.");
+
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = Convert.ToInt32(Console.ReadLine());
+
+            int sum = Data.mathOp(y, z);
+
+            
+       
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+            
+        }
+    }
+}
