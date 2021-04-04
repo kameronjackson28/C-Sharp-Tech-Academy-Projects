4e59273429ae4a643d2a5b3c9f71114838ce6ff3
kameron Jackson <kameronjackson28@gmail.com>
2021-04-04 14:05:03 -0500

    up

---
+++ Basic_C#_Programs/MethodClassSubmission/Math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClassSubmission
+{
+    //creates class with the name of "Math"
+    class Math
+    {
+        //creates void method myMath using two parameters
+        public void myMath(int firstNum, int secondNum)
+        {
+            //assigns a math operation on the first integer
+            int i  = firstNum * 25;
+            //displays second integer to screen
+            Console.WriteLine(secondNum);
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodClassSubmission/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MethodClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prints what's in parentheses to screen
+            Console.WriteLine("Hello, welcome to the beginning of the program");
+
+            //instantiates Math class
+            Math math = new Math();
+
+            //calls the method in the class passing in two numbers
+            math.myMath(4, 7);
+
+            //assigns value to the parameter variables
+            int firstNum = 4;
+            int secondNum = 7;
+            //calls the method in the class specifying the parameters by name
+            math.myMath(firstNum, secondNum);
+            
+        }
+    }
+}
