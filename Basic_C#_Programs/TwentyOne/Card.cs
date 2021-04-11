5dc5bdaf5302e52fb1ac0e9be8b526eb2b132c0b
kameron Jackson <kameronjackson28@gmail.com>
2021-04-11 15:40:53 -0500

    upload

--- Basic_C#_Programs/TwentyOne/Card.cs
+++ Basic_C#_Programs/TwentyOne/Card.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Channels;
 
 namespace TwentyOne
 {
@@ -8,6 +9,11 @@ namespace TwentyOne
     {
         public Suit Suit { get; set; }
         public Face Face { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} of {1}", Suit, Face);
+        }
     }
     public enum Suit
     {
