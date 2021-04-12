b4418c0b1ed474f60f74c2be3e946c2fe601bd69
kameron Jackson <kameronjackson28@gmail.com>
2021-04-12 08:31:27 -0500

    upload

--- Basic_C#_Programs/TwentyOne/Dealer.cs
+++ Basic_C#_Programs/TwentyOne/Dealer.cs
@@ -20,6 +20,7 @@ namespace TwentyOne
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\Kameron Jackson\OneDrive\Documents\GitHub\C-Sharp-Tech-Academy-Projects\Basic_C#_Programs\log.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
                 Deck.Cards.RemoveAt(0);
