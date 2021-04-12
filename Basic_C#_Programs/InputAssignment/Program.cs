073888ddbca6bd3e983edf49450a29796906752c
kameron Jackson <kameronjackson28@gmail.com>
2021-04-12 08:18:26 -0500

    upload

--- Basic_C#_Programs/InputAssignment/Program.cs
+++ Basic_C#_Programs/InputAssignment/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace InputAssignment
 {
@@ -6,7 +7,15 @@ namespace InputAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //prints what's in parentheses to screen
+            Console.WriteLine("Please input a number");
+            //assigns user input as string variable user
+            string user = Console.ReadLine();
+            //copies text to file path described using user variable as the argument.
+            File.WriteAllText(@"C:\\Users\\Kameron Jackson\\OneDrive\\Documents\\GitHub\\C-Sharp-Tech-Academy-Projects\\Basic_C#_Programs\\input.txt", user);
+
+            //prints user to screen.
+            Console.WriteLine(user);
         }
     }
 }
