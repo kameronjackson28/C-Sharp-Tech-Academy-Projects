cd1a21a1356463b75fbc684e48af4b482d50f3f2
kameron Jackson <kameronjackson28@gmail.com>
2021-04-13 15:39:47 -0500

    upload

--- Basic_C#_Programs/Constructors/Employee.cs
+++ Basic_C#_Programs/Constructors/Employee.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace Constructors
 {
     class Employee
-    {
+    { 
+        //uses "this" statement to chain this constructor with the latter
+        public Employee(string name) : this(name, 1234, "new hire")
+        {
+            
+        }
+        
+        //creates constructor Employee assigning it parameters of name, id, and department
+        public Employee(string name, int id, string department)
+        {
+            //assigns value to name variable
+            name = "Kameron";
+            //assigns value to department variable.
+            department = "Collections";
+        }
+        
     }
 }
--- Basic_C#_Programs/Constructors/Program.cs
+++ Basic_C#_Programs/Constructors/Program.cs
@@ -7,11 +7,10 @@ namespace Constructors
     {
         static void Main(string[] args)
         {
-            public static Employee(string name, int age)
-            {
-                name = "Kameron";
-                age = 25;
-            }
+            //creates a constant variable name payRate assigning it the value of 25
+            const int payRate = 25;
+            //assigns a variable using keyword var with value of a string.
+            var employeeBadgeName = "Kam Jackson";
            
 
         }
