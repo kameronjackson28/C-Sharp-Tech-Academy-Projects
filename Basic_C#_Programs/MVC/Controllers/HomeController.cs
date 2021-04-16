4d5260f34170df7f026ffdbdafb98eed268c41bd
kameron Jackson <kameronjackson28@gmail.com>
2021-04-15 20:51:33 -0500

    upload

---
+++ Basic_C#_Programs/MVC/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<string> names = new List<string>
+            {
+                "Jesse",
+                "Adam",
+                "Brett"
+            };
+            
+            return View(names);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Hello";
+
+            return View();
+        }
+    }
+}
